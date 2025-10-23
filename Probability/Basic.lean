a190f5f5215ab147df957e0fe44c63858fd97bf6
maeveburwell <91738977+maeveburwell@users.noreply.github.com>
2025-10-23 11:49:25 -0400

    Add comment to Probability/Basic.lean
    Added a comment to the Basic.lean file.

--- Probability/Basic.lean
+++ Probability/Basic.lean
@@ -13,7 +13,7 @@ import Mathlib.Data.Finset.Image
 import Mathlib.Algebra.Group.Pi.Basic -- for Pi.single
 
 --import Mathlib.Tactic.Explode
-
+-- Adding a comment
 open NNReal
 
 variable {τ : Type}
