76f42cfcc8750fa2077fe9b4dbdaa5f0c4fb9a6a
Marek Petrik <mpetrik@cs.unh.edu>
2025-11-04 11:51:10 -0500

    fixed build issues

--- Probability/Histories.lean
+++ Probability/Histories.lean
@@ -16,34 +16,14 @@ import Mathlib.Data.NNReal.Basic
 --import Mathlib.Data.Set.Basic
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Finset.Image
-import Mathlib.Logic.Function.Basic -- Injective
-
 import Mathlib.Probability.ProbabilityMassFunction.Basic
-
-
---import LeanMDPs.Finprob #can't do this yet bc I didn't import it
-------------------------------------------------
---instead I copied and pasted what I needed here
-/-- Finite probability distribution on a list (non-duplicates) -/
-structure Findist (Ω : List τ) : Type where
-  p : τ → ℝ                     -- probability measure
-  gezero : ∀ω ∈ Ω, 0 ≤ p ω      -- separate for convenience
-  sumsto : (Ω.map p).sum = 1    -- sums to 1
-  unique : List.Nodup Ω         -- the elements of Ω are unique
-
-abbrev Delta : List τ → Type := Findist
-abbrev Δ : List τ → Type := Delta
-------------------------------------------------
+import Probability.Basic
 
 namespace MDPs
 
-variable {σ α : Type}
---variable [Inhabited σ] [Inhabited α] -- used to construct policies
-
-open NNReal -- for ℝ≥0 notation
---open Finprob
-
 section Definitions
+
+open Findist
 
 /-- Markov decision process -/
 structure MDP : Type where
@@ -54,7 +34,7 @@ structure MDP : Type where
   A : ℕ
   A_ne : 0 < A
   /-- transition probability s, a, s' -/
-  P : Fin S → Fin A → Δ (List.finRange S)
+  P : Fin S → Fin A → Δ S
   /-- reward function s, a, s' -/
   r : Fin S → Fin A → Fin S → ℝ
 
