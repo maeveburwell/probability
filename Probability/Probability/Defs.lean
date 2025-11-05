05bbb315fd7fd6a67e0fa6319b9e26d1095c2bd3
Marek Petrik <mpetrik@cs.unh.edu>
2025-11-05 08:37:20 -0500

    fixed build issues

--- Probability/Probability/Defs.lean
+++ Probability/Probability/Defs.lean
@@ -1,10 +1,13 @@
 import Probability.Probability.Prelude
 
+--------------------------- Findist ---------------------------------------------------------------<F2>
+
 /-- Finite probability distribution on a set-like list (non-duplicates)  -/
 structure Findist (N : ℕ)  : Type where
   ℙ : List ℚ                      -- probabilities
   simplex : LSimplex ℙ            -- proof of a measure
   lmatch : ℙ.length = N           -- correct length of probability
+
 
 namespace Findist
 
@@ -87,12 +90,6 @@ theorem not_degen_supp (supp : ¬P.degenerate) : P.supported :=
 theorem degen_of_not_supp (notsupp : ¬P.supported) : P.degenerate :=
   by simp_all [Finprob.degenerate, Finprob.supported]
 
-def shrink (supp : P.supported) : Finprob :=
-  {ℙ := P.ℙ.shrink, prob := P.prob.shrink supp}
-
-
--- Define an induction principle for probability spaces
--- similar  to the induction on lists, but also must argue about probability distributions
 
 theorem nonempty : ¬P.ℙ.isEmpty :=
   by intro a;
@@ -108,14 +105,6 @@ theorem length_gt_zero : P.length ≥ 1 :=
       exact P.nonempty this
     exact Nat.succ_le_of_lt (List.length_pos_iff.mpr hne)
 
-theorem shrink_length (supp : P.supported) : (P.shrink supp).length = P.length - 1 :=
-    by  have h := Finprob.nonempty P
-        simp [List.isEmpty] at h
-        simp! [Finprob.shrink, Finprob.length, List.shrink, LSimplex.shrink]
-
-theorem shrink_length_lt (supp : P.supported) : (P.shrink supp).length < P.length :=
-    by rw [Finprob.shrink_length P supp]
-       exact Nat.sub_one_lt_of_lt (Finprob.length_gt_zero P)
 
 theorem nonempty_P : P.ℙ ≠ [] := P.prob.nonempty
 
@@ -204,3 +193,144 @@ theorem ind_zero_one (cond : τ → Bool) : ( (𝕀∘cond) ω = 1) ∨ ((𝕀�
 
 
 end RandomVariable
+
+
+------------------------------ Section Probability ---------------------------
+
+section Probability
+
+----- standard probability
+
+
+variable (P : Finprob) (B : FinRV Bool) (C : FinRV Bool)
+
+/-- Probability of B -/
+def probability : ℚ :=  P.ℙ.iprodb B
+
+notation "ℙ[" B "//" P "]" => probability P B
+
+/-- Conditional probability of B -/
+def probability_cnd : ℚ := ℙ[ B ∧ᵣ C // P ] / ℙ[ C // P ]
+
+--- main decomposition properties
+
+theorem Prob.true_one : ℙ[ fun _ ↦ true // P] = 1 :=
+    by simp only [probability]; rw [List.iprodb_true_sum]; exact P.prob.normalized
+
+--- sums
+
+theorem List.list_compl_sums_to_one (L : List ℚ) : L.iprodb B + L.iprodb (B.not) = L.sum :=
+  by induction L with
+     | nil => simp [List.iprodb]
+     | cons head tail =>
+        simp [List.iprodb]
+        cases (B tail.length)
+        · simp; linarith
+        · simp; linarith
+
+
+theorem List.law_of_total_probs (L : List ℚ)  : L.iprodb B = L.iprodb (B ∧ᵣ C) + L.iprodb (B ∧ᵣ (¬ᵣC) ) :=
+    by induction L with
+       | nil => simp [List.iprodb]
+       | cons head tail =>
+          simp [List.iprodb]
+          cases bB: B tail.length
+          · cases bC : C tail.length; simp_all; simp_all
+          · cases bC : C tail.length
+            · simp_all; ring;
+            · simp_all; ring;
+
+theorem Prob.prob_compl_sums_to_one : ℙ[B // P] + ℙ[¬ᵣB // P] = 1 :=
+  calc
+    ℙ[ B // P ] + ℙ[ ¬ᵣB // P] = P.ℙ.sum := P.ℙ.list_compl_sums_to_one B
+    _ = 1 := P.prob.normalized
+
+theorem Prob.prob_compl_one_minus : ℙ[¬ᵣB // P] = 1 - ℙ[B // P] :=
+    by have := Prob.prob_compl_sums_to_one P B
+       linarith
+
+
+theorem Prob.law_of_total_probs : ℙ[B // P] = ℙ[ B ∧ᵣ C // P] + ℙ[ B ∧ᵣ ¬ᵣC //P] := P.ℙ.law_of_total_probs B C
+
+---- conditional probability
+
+notation "ℙ[" B "|" C "//" P "]" => probability_cnd P B C
+
+theorem Prob.conditional_total (h : 0 < ℙ[C // P]) : ℙ[B ∧ᵣ C // P] =  ℙ[ B | C // P] * ℙ[ C // P] :=
+  by simp [probability_cnd] at ⊢ h
+     have : P.ℙ.iprodb C * (P.ℙ.iprodb C)⁻¹ = 1 := Rat.mul_inv_cancel (P.ℙ.iprodb C) (Ne.symm (ne_of_lt h))
+     calc
+        P.ℙ.iprodb (B ∧ᵣC) = P.ℙ.iprodb (B ∧ᵣC) * 1 := by ring
+        _ = P.ℙ.iprodb (B ∧ᵣC) * (P.ℙ.iprodb C * (P.ℙ.iprodb C)⁻¹) := by rw [←this]
+        _ = P.ℙ.iprodb (B ∧ᵣ C) / P.ℙ.iprodb C * P.ℙ.iprodb C := by ring
+
+
+theorem Prob.law_of_total_probs_cnd
+  (h1 : 0 < ℙ[C // P]) (h2 : ℙ[C // P] < 1)  : ℙ[B // P] = ℙ[B | C // P] * ℙ[ C // P] + ℙ[B | ¬ᵣC // P] * ℙ[¬ᵣC // P] :=
+        by have h2' : 0 < ℙ[¬ᵣC // P] := by rw [prob_compl_one_minus]; linarith
+           rw [←Prob.conditional_total P B C h1]
+           rw [←Prob.conditional_total P B (¬ᵣC) h2']
+           exact law_of_total_probs P B C
+
+end Probability
+
+section Expectations
+
+def List.iprod (ℙ : List ℚ) (X : FinRV ℚ) : ℚ :=
+    match ℙ with
+    | [] => 0
+    | head :: tail =>  head * (X tail.length) + tail.iprod X
+
+
+variable (P : Finprob) (X Y Z: FinRV ℚ) (B : FinRV Bool)
+
+def expect : ℚ := P.ℙ.iprod X
+
+notation "𝔼[" X "//" P "]" => expect P X
+
+-- expectation for a joint probability space and random variable
+notation "𝔼[" PX "]" => expect PX.1 PX.2
+
+
+
+-- conditional expectation
+
+def expect_cnd : ℚ := P.ℙ.iprod X / P.ℙ.iprodb B
+
+notation "𝔼[" X "|" B "//" P "]" => expect_cnd P X B
+
+-- expectation for a joint probability space and random variable
+notation "𝔼[" PX "]" => expect PX.1 PX.2
+notation "𝔼[" PX "|" B "]" => expect_cnd PX.1 PX.2 B
+
+-- conditional expectation: conditioning on a random variable: this defintion creates a probability
+-- space and a random variable
+
+variable {K : ℕ} (D : FinRV (Fin K.succ))  -- a discrete random variable with K+1 values
+
+theorem List.law_of_total_expectations (L : List ℚ) (X : FinRV ℚ) (B : FinRV Bool) :
+  L.iprod X = L.iprod (fun ω => if B ω then X ω else 0) + L.iprod (fun ω => if ¬B ω then X ω else 0) :=
+  by induction L with
+     | nil => simp [List.iprod]
+     | cons head tail =>
+        simp [List.iprod]
+        cases bB: B tail.length
+        · simp_all; ring
+        · simp_all; ring
+
+theorem Prob.law_of_total_expectation (P : Finprob) (X : FinRV ℚ) (B : FinRV Bool)
+  (h1 : 0 < ℙ[B // P]) (h2 : 0 < ℙ[¬ᵣB // P]) :
+  𝔼[X // P] = 𝔼[X | B // P] * ℙ[B // P] + 𝔼[X | ¬ᵣB // P] * ℙ[¬ᵣB // P] :=
+  by
+    simp [expect, expect_cnd] at ⊢ h1 h2
+    have h1' : P.ℙ.iprodb B ≠ 0 := Ne.symm (ne_of_lt h1)
+    have h2' : P.ℙ.iprodb (¬ᵣB) ≠ 0 := Ne.symm (ne_of_lt h2)
+
+    have h3' : P.ℙ.iprod X = P.ℙ.iprod (fun ω => if B ω then X ω else 0) + P.ℙ.iprod (fun ω => if ¬B ω then X ω else 0) :=
+      List.law_of_total_expectations P.ℙ X B
+    rw [h3']
+    simp_all
+    sorry
+
+end Expectations
+
