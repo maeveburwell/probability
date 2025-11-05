d0b6fa9f452e5a3df0f605595168101b4c260228
Marek Petrik <mpetrik@cs.unh.edu>
2025-11-04 20:26:24 -0500

    Reorg files

--- Probability.lean
+++ Probability.lean
@@ -1,9 +1,7 @@
 -- This module serves as the root of the `Probability` library.
 -- Import modules here that should be built as part of the library.
 
-import Probability.Prob
-import Probability.List
-import Probability.LSimplex
+import Probability.Prelude
 import Probability.Findist
 import Probability.Finprob
 import Probability.FinRV
