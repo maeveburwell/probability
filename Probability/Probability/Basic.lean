85e54c43c6e700830f515eef802f02b034082bef
Marek Petrik <mpetrik@cs.unh.edu>
2025-11-21 17:21:05 -0500

    thoughts and failed proofs

--- Probability/Probability/Basic.lean
+++ Probability/Probability/Basic.lean
@@ -93,11 +93,13 @@ variable {K : ℕ}  {L : FinRV n (Fin K)}
 variable {pmf : Fin K → ℚ}
 variable {P : Findist n}
 
---is the theorem below true?
-theorem pmf_rv_k_ge_1 (h : PMF pmf P L)  : 0 < K := sorry
-  -- match K with
-  -- | Nat.zero => Fin.elim0 (L 0)
-  -- | Nat.succ n => Nat.succ_pos n
+-- em: is the theorem below true? ma: Hmm, I think it is no longer true; I wonder why it was true in the 
+-- first place before
+theorem pmf_rv_k_ge_1 (h : PMF pmf P L)  : 0 < K :=
+  by cases' K with  z nz  
+     · unfold PMF at h 
+       sorry 
+     · exact Nat.zero_lt_succ z
 
 end PMF
 
