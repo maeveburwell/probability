79a9a82a5a2e91e591715d38ac2cc17482ded06a
Marek Petrik <mpetrik@cs.unh.edu>
2026-02-02 15:56:28 -0500

    formatting

--- Probability/Probability/Quantile.lean
+++ Probability/Probability/Quantile.lean
@@ -6,8 +6,6 @@ namespace Statistic
 
 section Definition 
 
-variable {n : ℕ}
-
 --def UnitI := {α : ℚ // 0 ≤ α ∧ α ≤ 1}
 
 variable {n : ℕ} (P : Findist n) (X Y : FinRV n ℚ) (α : ℚ) (q v : ℚ)
@@ -26,18 +24,18 @@ def QuantileLower : Set ℚ := {q | IsQuantileLower P X α q}
 
 end Definition
 
-theorem qset_lb : q ∈ Quantile P X α → ℙ[ X ≤ᵣ q // P ] ≥ α := by simp_all [Quantile, IsQuantile]
+theorem qset_lb : q ∈ Quantile P X α → ℙ[X ≤ᵣ q // P ] ≥ α := by simp_all [Quantile, IsQuantile]
 
-theorem qset_ub : q ∈ Quantile P X α → ℙ[ X ≥ᵣ q // P] ≥ 1 - α := by simp_all [Quantile, IsQuantile]
+theorem qset_ub : q ∈ Quantile P X α → ℙ[X ≥ᵣ q // P] ≥ 1 - α := by simp_all [Quantile, IsQuantile]
 
-theorem qset_def : q ∈ Quantile P X α ↔ ℙ[ X ≤ᵣ q // P ] ≥ α ∧ ℙ[ X ≥ᵣ q // P] ≥ 1 - α := by simp_all [Quantile, IsQuantile]
+theorem qset_def : q ∈ Quantile P X α ↔ ℙ[X ≤ᵣ q // P ] ≥ α ∧ ℙ[X ≥ᵣ q // P] ≥ 1 - α := by simp_all [Quantile, IsQuantile]
 
 theorem qset_not_def : q ∉ Quantile P X α ↔ ℙ[ X ≤ᵣ q // P ] < α ∨ ℙ[ X ≥ᵣ q // P] < 1 - α := by
     constructor; repeat intro h2; grind [qset_def]
 
-theorem qsetlower_def : q ∈ QuantileLower P X α ↔ ℙ[ X ≥ᵣ q // P] ≥ 1 - α := by simp_all [QuantileLower, IsQuantileLower]
+theorem qsetlower_def : q ∈ QuantileLower P X α ↔ ℙ[X ≥ᵣ q // P] ≥ 1 - α := by simp_all [QuantileLower, IsQuantileLower]
 
-theorem qsetlower_def_lt : q ∈ QuantileLower P X α ↔ ℙ[ X <ᵣ q // P] ≤ α :=
+theorem qsetlower_def_lt : q ∈ QuantileLower P X α ↔ ℙ[X <ᵣ q // P] ≤ α :=
     by constructor
        · intro h; have := qsetlower_def.mp h; rw [prob_lt_of_ge]; linarith
        · intro h; rw [prob_lt_of_ge] at h;
@@ -53,12 +51,12 @@ theorem qset_ub_lt : q ∈ Quantile P X α → ℙ[ X <ᵣ q // P] ≤ α :=
 theorem qset_of_cond : ℙ[ X ≤ᵣ q // P ] ≥ α ∧ ℙ[ X ≥ᵣ q // P] ≥ 1 - α → q ∈ Quantile P X α :=
     by intro h; simp_all [Quantile, IsQuantile]
 
-theorem qset_of_cond_lt : ℙ[ X ≤ᵣ q // P ] ≥ α ∧ ℙ[ X <ᵣ q // P] ≤ α → q ∈ Quantile P X α :=
+theorem qset_of_cond_lt : ℙ[X ≤ᵣ q // P] ≥ α ∧ ℙ[ X <ᵣ q // P] ≤ α → q ∈ Quantile P X α :=
     by intro h1
        have h2 : ℙ[ X ≥ᵣ q // P] ≥ 1 - α := by rw [prob_ge_of_lt]; linarith
        exact qset_of_cond ⟨h1.1, h2⟩
 
-theorem qsetlower_of_cond : ℙ[ X ≤ᵣ q // P ] ≥ α ∧ ℙ[ X ≥ᵣ q // P] ≥ 1 - α → q ∈ QuantileLower P X α :=
+theorem qsetlower_of_cond : ℙ[X ≤ᵣ q // P] ≥ α ∧ ℙ[ X ≥ᵣ q // P] ≥ 1 - α → q ∈ QuantileLower P X α :=
     by intro h; simp_all [QuantileLower, IsQuantileLower]
 
 theorem qsetlower_of_cond_lt : ℙ[ X ≤ᵣ q // P ] ≥ α ∧ ℙ[ X <ᵣ q // P] ≤ α → q ∈ QuantileLower P X α :=
@@ -72,7 +70,6 @@ theorem quantile_implies_quantilelower : IsQuantile P X α v → IsQuantileLower
 
 theorem quantile_subset_quantilelower : Quantile P X α ⊆ QuantileLower P X α := fun _ => quantile_implies_quantilelower
 
-
 theorem quantile_le_monotone : X ≤ Y → IsCofinalFor (QuantileLower P X α) (IsQuantileLower P Y α) := by
   intro hle q₁ hvar₁
   have hq₁ := le_refl q₁
@@ -84,11 +81,11 @@ theorem quantilelower_cashinv : q ∈ QuantileLower P X α ↔ (q+c) ∈ Quantil
   · intro h; rw [qsetlower_def, prob_ge_cashinvar c] at h; exact h
   · intro h; rw [qsetlower_def, prob_ge_cashinvar c]; exact h
 
+/-- Adding a constant to a random variable shifts the quantile -/
 theorem quantilelower_cash_image : QuantileLower P (X+c•1) α = (fun x ↦ x+c) '' QuantileLower P X α := by
   apply Set.eq_of_subset_of_subset
   · unfold Set.image
     intro qc hqc
-    --rw [quantilelower_cashinv (c:=c)] at hq
     use qc-c
     constructor
     · generalize hqcq : qc - c = q
